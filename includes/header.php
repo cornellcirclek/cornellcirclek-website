5295d0f279f8f597633a504e8da47beb6dd3debb
Brendon Nguyen <brendonnguyen@Brendons-MacBook-Air.local>
2020-05-10 00:20:03 -0500

    starter php files (harvested from 2300)

---
+++ includes/header.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="styles/all.css">
+
+<header>
+</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include('includes/init.php');
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+  <?php include("includes/header.php");?>
+
+
+</body>
+</html>
